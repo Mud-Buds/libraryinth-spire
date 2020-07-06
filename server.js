0ba8077de27363cb1df8abb04c3d6a366e8310dd
Logan Scott <57209519+logantscott@users.noreply.github.com>
2020-07-06 11:54:57 -0600

    Features/basic parsing (#5)
    * added public/index.html client, updated app.js/server.js, basic User model
    
    * added public/index.html client, updated app.js/server.js, basic User model
    
    * added second room/emitter, and updated index to split game/chat
    
    * scratchpad basic parsing
    
    Co-authored-by: Rachel Donahue <racheldonahuedev@gmail.com>

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 7890;
-
 const http = app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
-
 const io = require('socket.io').listen(http);
+
+const { gameParser, chatParser } = require('./lib/helpers/parser');
 
 io.on('connection', (socket) => {
   // console.log(socket);
@@ -26,37 +26,20 @@ io.on('connection', (socket) => {
   
   // display the Message of the Day
   const displayMOTD = setTimeout(() => {
-    socket.emit('chat', { msg: 'Welcome to the Libraryinth Spire!', color: 'blue' });
+    socket.emit('chat', { msg: 'Welcome to the Libraryinth Spire!', color: 'skyblue' });
   }, 3000);
 
   // emit message to all when receiving message from client
   socket.on('chat', (input) => {
     io.emit('chat', { msg: socket.id.slice(-3) + ': ' + input });
+  });
 
-
-    // call the parser here
-    // parser(data)
-    //   .then(response => {
-    //     // parser promise.resolve()
-    //     switch(response.type){
-    //       case 'say':
-    //         io.emit('chat', { msg: 'Guest' + socket.id.slice(-3) + ': ' + response.message });
-    //         break;
-    //       case 'whisper':
-    //         io.to(response.toUser).emit('chat', { 
-    //           msg: 'from Guest' + socket.id.slice(-3) + ': ' + response.message,
-    //           color: 'red',
-    //           background: 'black'
-    //         });
-    //         break;
-    //       default:
-    //         io.emit('chat', { msg: 'Guest' + socket.id.slice(-3) + ': ' + response.message });
-    //     }
-    //   })
-    //   .catch(() => {
-    //     // parser promise.reject()
-    //     io.emit('chat', { msg: 'bad syntax' });
-    //   });
+  socket.on('game', (input) => {
+    gameParser(input)
+      .then(res => socket.emit('game', res))
+      .catch(res => {
+        socket.emit('game', res);
+      });
   });
 
   socket.on('game', () => {
@@ -69,3 +52,28 @@ io.on('connection', (socket) => {
     clearTimeout(displayMOTD);
   });
 });
+
+
+// call the parser here
+// parser(data)
+//   .then(response => {
+//     // parser promise.resolve()
+//     switch(response.type){
+//       case 'say':
+//         io.emit('chat', { msg: 'Guest' + socket.id.slice(-3) + ': ' + response.message });
+//         break;
+//       case 'whisper':
+//         io.to(response.toUser).emit('chat', { 
+//           msg: 'from Guest' + socket.id.slice(-3) + ': ' + response.message,
+//           color: 'red',
+//           background: 'black'
+//         });
+//         break;
+//       default:
+//         io.emit('chat', { msg: 'Guest' + socket.id.slice(-3) + ': ' + response.message });
+//     }
+//   })
+//   .catch(() => {
+//     // parser promise.reject()
+//     io.emit('chat', { msg: 'bad syntax' });
+//   });
