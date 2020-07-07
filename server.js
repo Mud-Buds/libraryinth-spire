73edaea3f8a0619c3d4e8c1b41d19cb35e77dfad
Logan Scott <logantscott@gmail.com>
2020-07-07 17:17:03 -0600

    html for gamemessages, updated front-end styles for better split on game/chat

--- server.js
+++ server.js
@@ -22,24 +22,42 @@ const http = app.listen(PORT, () => {
 const io = require('socket.io').listen(http);
 
 const { gameParser, chatParser } = require('./lib/helpers/parser');
-const { Mongoose } = require('mongoose');
 
 io.on('connection', (socket) => {
   // console.log(socket);
   console.log(`${socket.id} connected`);
+  io.emit('chat', { msg: 'guest-' + socket.id.slice(0, 4) + ' connected' });
+
   // console.log(socket.id);
 
+  const motdTitle = '  ooooo         o8o   .o8                                                o8o                  .   oooo <br />\
+  `888\'         `"\'  "888                                                `"\'                .o8   `888 <br />\
+   888         oooo   888oooo.  oooo d8b  .oooo.   oooo d8b oooo    ooo oooo  ooo. .oo.   .o888oo  888 .oo. <br />\
+   888         `888   d88\' `88b `888""8P `P  )88b  `888""8P  `88.  .8\'  `888  `888P"Y88b    888    888P"Y88b <br />\
+   888          888   888   888  888      .oP"888   888       `88..8\'    888   888   888    888    888   888 <br />\
+   888       o  888   888   888  888     d8(  888   888        `888\'     888   888   888    888 .  888   888 <br />\
+  o888ooooood8 o888o  `Y8bod8P\' d888b    `Y888""8o d888b        .8\'     o888o o888o o888o   "888" o888o o888o <br />\
+                                                            .o..P\' <br />\
+                                                            `Y8P\' <br />\
+<br />\
+                                 .oooooo..o             o8o <br />\
+                                d8P\'    `Y8             `"\' <br />\
+                                Y88bo.      oo.ooooo.  oooo  oooo d8b  .ooooo. <br />\
+                                 `"Y8888o.   888\' `88b `888  `888""8P d88\' `88b <br />\
+                                     `"Y88b  888   888  888   888     888ooo888 <br />\
+                                oo     .d8P  888   888  888   888     888    .o <br />\
+                                8""88888P\'   888bod8P\' o888o d888b    `Y8bod8P\' <br />\
+                                             888 <br />\
+                                            o888o';
 
-  // socket.join(socket.id);
-  
   // display the Message of the Day
   const displayMOTD = setTimeout(() => {
-    socket.emit('chat', { msg: 'Welcome to the Libraryinth Spire!', color: 'skyblue' });
-  }, 3000);
+    socket.emit('game', { msg: '<span style="font-size: 10px; color: blue; white-space: pre; font-family: monospace">' + motdTitle + '</span><br /><br /><span style="color:white">MOTD goes here.</span><hr /><br />', color: 'skyblue' });
+  }, 2000);
 
   // emit message to all when receiving message from client
   socket.on('chat', (input) => {
-    io.emit('chat', { msg: socket.id.slice(-3) + ': ' + input });
+    io.emit('chat', { msg: 'guest-' + socket.id.slice(0, 4) + ': ' + input });
   });
 
   socket.on('game', (input) => {
