40c716e4b2d48261cefc75e9ed9eca9ad60615b3
Logan Scott <57209519+logantscott@users.noreply.github.com>
2020-07-08 17:59:48 -0600

    Features/user login (#30)
    * login kinda working..., using User.authorize and setting socket.request.user
    
    * cleaning up server.js
    
    * signup route mostly working via /signup <username> <password>
    
    * whispers and reload auth
    
    * signup/login messages updated, most errors seem to be caught properly and displayed
    
    * removed/commented unused eslint errors

---
+++ motd.js
@@ -0,0 +1,19 @@
+module.exports = '  ooooo         o8o   .o8                                                o8o                  .   oooo <br />\
+  `888\'         `"\'  "888                                                `"\'                .o8   `888 <br />\
+   888         oooo   888oooo.  oooo d8b  .oooo.   oooo d8b oooo    ooo oooo  ooo. .oo.   .o888oo  888 .oo. <br />\
+   888         `888   d88\' `88b `888""8P `P  )88b  `888""8P  `88.  .8\'  `888  `888P"Y88b    888    888P"Y88b <br />\
+   888          888   888   888  888      .oP"888   888       `88..8\'    888   888   888    888    888   888 <br />\
+   888       o  888   888   888  888     d8(  888   888        `888\'     888   888   888    888 .  888   888 <br />\
+  o888ooooood8 o888o  `Y8bod8P\' d888b    `Y888""8o d888b        .8\'     o888o o888o o888o   "888" o888o o888o <br />\
+                                                            .o..P\' <br />\
+                                                            `Y8P\' <br />\
+<br />\
+                                 .oooooo..o             o8o <br />\
+                                d8P\'    `Y8             `"\' <br />\
+                                Y88bo.      oo.ooooo.  oooo  oooo d8b  .ooooo. <br />\
+                                 `"Y8888o.   888\' `88b `888  `888""8P d88\' `88b <br />\
+                                     `"Y88b  888   888  888   888     888ooo888 <br />\
+                                oo     .d8P  888   888  888   888     888    .o <br />\
+                                8""88888P\'   888bod8P\' o888o d888b    `Y8bod8P\' <br />\
+                                             888 <br />\
+                                            o888o';
